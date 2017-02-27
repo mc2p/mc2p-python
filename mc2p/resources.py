b1ee646beb1b3bcd1331e64083f9523b01d63a4e
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-27 17:26:19 +0100

    Updated resources to linked with objects

--- mc2p/resources.py
+++ mc2p/resources.py
@@ -1,42 +1,53 @@
 from .base import Resource, ReadOnlyResource, CreateReadOnlyResource
+from .objects import Product, Plan, Tax, Shipping, Coupon, Transaction, Subscription, Sale, Currency, Gateway
 
 
 class ProductResource(Resource):
     URL = '/product/'
+    OBJECT_ITEM_CLASS = Product
 
 
 class PlanResource(Resource):
     URL = '/plan/'
+    OBJECT_ITEM_CLASS = Plan
 
 
 class TaxResource(Resource):
     URL = '/tax/'
+    OBJECT_ITEM_CLASS = Tax
 
 
 class ShippingResource(Resource):
     URL = '/shipping/'
+    OBJECT_ITEM_CLASS = Shipping
 
 
 class CouponResource(Resource):
     URL = '/coupon/'
+    OBJECT_ITEM_CLASS = Coupon
 
 
 class TransactionResource(CreateReadOnlyResource):
     URL = '/transaction/'
+    OBJECT_ITEM_CLASS = Transaction
 
 
 class SubscriptionResource(CreateReadOnlyResource):
     URL = '/subscription/'
+    OBJECT_ITEM_CLASS = Subscription
 
 
 class SaleResource(ReadOnlyResource):
     URL = '/sale/'
+    OBJECT_ITEM_CLASS = Sale
 
 
 class CurrencyResource(ReadOnlyResource):
     URL = '/currency/'
+    OBJECT_ITEM_CLASS = Currency
 
 
 class GatewayResource(ReadOnlyResource):
     URL = '/gateway/'
+    OBJECT_ITEM_CLASS = Gateway
 
