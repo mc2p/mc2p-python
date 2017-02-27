7d6d1768396e0f12bf76a203af228e6fda1c9ca7
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-27 13:22:41 +0100

    Added __init__ with metadata

---
+++ mc2p/__init__.py
@@ -0,0 +1,14 @@
+__title__ = 'MyChoice2Pay Python'
+__version__ = '0.0.1'
+__author__ = 'MyChoice2Pay'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2017 MyChoice2Pay'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
+
+# Default datetime input and output formats
+ISO_8601 = 'iso-8601'
