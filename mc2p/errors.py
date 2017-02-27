58b3089e0e53dd5e002c3bd7e61c05899e276b5a
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-27 19:15:51 +0100

    Improved str of errors

--- mc2p/errors.py
+++ mc2p/errors.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class MC2PError(Exception):
 
     def __init__(self, message=None, json_body=None, resource=None, resource_id=None):
@@ -7,6 +10,16 @@ class MC2PError(Exception):
         self.json_body = json_body
         self.resource = resource
         self.resource_id = resource_id
+
+    def __unicode__(self):
+        return u"Message: {0}, Body: {1}".format(self._message, self.json_body)
+
+    if sys.version_info > (3, 0):
+        def __str__(self):
+            return self.__unicode__()
+    else:
+        def __str__(self):
+            return unicode(self).encode('utf-8')
 
 
 class InvalidRequestError(MC2PError):
