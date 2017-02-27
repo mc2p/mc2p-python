85bfefb31a2882737b6dd2201df27218027c1d1a
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-27 19:16:13 +0100

    Allowed create objects directly

--- mc2p/mc2p.py
+++ mc2p/mc2p.py
@@ -1,6 +1,13 @@
 from .request import APIRequest
 from .resources import ProductResource, PlanResource, TaxResource, ShippingResource, CouponResource, \
     TransactionResource, SubscriptionResource, SaleResource, CurrencyResource, GatewayResource
+from .objects import Product, Plan, Tax, Shipping, Coupon, Transaction, Subscription, Sale, Currency, Gateway
+
+
+def class_decorator(cls, resource):
+    def init(json_dict):
+        return cls(json_dict, resource)
+    return init
 
 
 class MC2P(object):
@@ -17,3 +24,14 @@ class MC2P(object):
         self.sale = SaleResource(self.api_request)
         self.currency = CurrencyResource(self.api_request)
         self.gateway = GatewayResource(self.api_request)
+
+        self.Product = class_decorator(Product, self.product)
+        self.Plan = class_decorator(Plan, self.plan)
+        self.Tax = class_decorator(Tax, self.tax)
+        self.Shipping = class_decorator(Shipping, self.shipping)
+        self.Coupon = class_decorator(Coupon, self.coupon)
+        self.Transaction = class_decorator(Transaction, self.transaction)
+        self.Subscription = class_decorator(Subscription, self.subscription)
+        self.Sale = class_decorator(Sale, self.sale)
+        self.Currency = class_decorator(Currency, self.currency)
+        self.Gateway = class_decorator(Gateway, self.gateway)
