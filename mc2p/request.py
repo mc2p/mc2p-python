4506d38ba08773b93d08237b94bf7c3907921d78
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-28 16:58:55 +0100

    Added docs to api_request file

--- mc2p/request.py
+++ mc2p/request.py
@@ -5,10 +5,18 @@ import requests
 
 
 class APIRequest(object):
+    """
+    API request - class used to connect with the API
+    """
     AUTHORIZATION_HEADER = 'AppKeys'
     API_URL = 'api.mychoice2pay.com'
 
     def __init__(self, key, secret_key):
+        """
+        Initializes an api request
+        :param key: key to connect with API
+        :param secret_key: secret key to connect with API
+        """
         self.key = key
         self.secret_key = secret_key
 
@@ -19,6 +27,10 @@ class APIRequest(object):
 
     @property
     def headers(self):
+        """
+        Creates the headers to include in the request
+        :return: A dictionary with the headers needed for the API
+        """
         return {
             'authorization': '%s %s:%s' % (
                 self.AUTHORIZATION_HEADER,
@@ -29,12 +41,22 @@ class APIRequest(object):
         }
 
     def get_abs_url(self, url):
+        """
+        :param url: relative url
+        :return: The absolute url to send the request
+        """
         return 'https://%s%s' % (
             self.API_URL,
             url
         )
 
     def _request(self, method, status_code=200):
+        """
+        Decorator to make the request based on the method received
+        :param method: method to make the request
+        :param status_code: value to check if the request receive a correct response
+        :return: a function to make the request
+        """
         def func(rel_url=None, data=None, abs_url=None, resource=None, resource_id=None):
             request = requests.request(
                 method,
