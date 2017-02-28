a2faa93f81953c8aafaf6b60d753a2279482eb84
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-28 16:57:04 +0100

    Added docs to objects and resources files

--- mc2p/objects.py
+++ mc2p/objects.py
@@ -2,40 +2,70 @@ from .base import RetrieveObjectItem, DeleteSaveRetrieveObjectItem, CreateRetrie
 
 
 class Product(DeleteSaveRetrieveObjectItem):
+    """
+    Product object
+    """
     pass
 
 
 class Plan(DeleteSaveRetrieveObjectItem):
+    """
+    Plan object
+    """
     pass
 
 
 class Tax(DeleteSaveRetrieveObjectItem):
+    """
+    Tax object
+    """
     pass
 
 
 class Shipping(DeleteSaveRetrieveObjectItem):
+    """
+    Shipping object
+    """
     pass
 
 
 class Coupon(DeleteSaveRetrieveObjectItem):
+    """
+    Coupon object
+    """
     pass
 
 
 class Transaction(CreateRetrieveObjectItem):
+    """
+    Transaction object
+    """
     pass
 
 
 class Subscription(CreateRetrieveObjectItem):
+    """
+    Subscription object
+    """
     pass
 
 
 class Sale(RetrieveObjectItem):
+    """
+    Sale object
+    """
     pass
 
 
 class Currency(RetrieveObjectItem):
+    """
+    Currency object
+    """
     pass
 
 
 class Gateway(RetrieveObjectItem):
+    """
+    Gateway object
+    """
     pass
--- mc2p/resources.py
+++ mc2p/resources.py
@@ -3,51 +3,81 @@ from .objects import Product, Plan, Tax, Shipping, Coupon, Transaction, Subscrip
 
 
 class ProductResource(DeleteChangeCreateListDetailResource):
+    """
+    Product resource
+    """
     URL = '/product/'
     OBJECT_ITEM_CLASS = Product
 
 
 class PlanResource(DeleteChangeCreateListDetailResource):
+    """
+    Plan resource
+    """
     URL = '/plan/'
     OBJECT_ITEM_CLASS = Plan
 
 
 class TaxResource(DeleteChangeCreateListDetailResource):
+    """
+    Tax resource
+    """
     URL = '/tax/'
     OBJECT_ITEM_CLASS = Tax
 
 
 class ShippingResource(DeleteChangeCreateListDetailResource):
+    """
+    Shipping resource
+    """
     URL = '/shipping/'
     OBJECT_ITEM_CLASS = Shipping
 
 
 class CouponResource(DeleteChangeCreateListDetailResource):
+    """
+    Coupon resource
+    """
     URL = '/coupon/'
     OBJECT_ITEM_CLASS = Coupon
 
 
 class TransactionResource(CreateListDetailResource):
+    """
+    Transaction resource
+    """
     URL = '/transaction/'
     OBJECT_ITEM_CLASS = Transaction
 
 
 class SubscriptionResource(CreateListDetailResource):
+    """
+    Subscription resource
+    """
     URL = '/subscription/'
     OBJECT_ITEM_CLASS = Subscription
 
 
 class SaleResource(ListDetailResource):
+    """
+    Sale resource
+    """
     URL = '/sale/'
     OBJECT_ITEM_CLASS = Sale
 
 
 class CurrencyResource(ListDetailResource):
+    """
+    Currency resource
+    """
     URL = '/currency/'
     OBJECT_ITEM_CLASS = Currency
 
 
 class GatewayResource(ListDetailResource):
+    """
+    Gateway resource
+    """
     URL = '/gateway/'
     OBJECT_ITEM_CLASS = Gateway
 
