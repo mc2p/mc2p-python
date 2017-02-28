4b6d1130f89526b96e67a1325e67c0041ae86319
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-02-28 16:56:45 +0100

    Added docs to utils file

--- mc2p/utils.py
+++ mc2p/utils.py
@@ -2,6 +2,12 @@ from .errors import BadUseError
 
 
 def id_required_and_not_deleted(func):
+    """
+    Decorator to check if the json_dict have an id and
+    the object was not deleted
+    :param func: function to decorate
+    :return: function decorated
+    """
     def result(self, *args, **kwargs):
         if not self.json_dict.get(self.ID_PROPERTY):
             raise BadUseError('Object don\'t have ID')
